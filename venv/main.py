7393403fac2a62eff6bba6182b8286482d577d88
HyrekanDragon <HyrekanDragon@gmail.com>
2019-10-12 14:04:09 -0700

    Added Mario

--- venv/main.py
+++ venv/main.py
@@ -1,19 +1,35 @@
 import sys
 import pygame
+from time import sleep
+from player import Mario
 
 
 def run_game():
     pygame.init()
     screen = pygame.display.set_mode((300, 300))
     pygame.display.set_caption("Insert Title Here")
+    mario = Mario(screen)
 
     while True:
-        screen.fill((255, 255, 255))
-        pygame.display.flip()
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    mario.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    mario.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    mario.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    mario.moving_left = False
+
+        screen.fill((255, 255, 255))
+        mario.update()
+        mario.blitme()
+        sleep(0.001)
+        pygame.display.flip()
 
 
 run_game()
---
+++ venv/player.py
@@ -0,0 +1,35 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Mario(Sprite):
+    def __init__(self, screen):
+        super(Mario, self).__init__()
+        self.screen = screen
+
+        # Load Mario standing image
+        self.image = pygame.image.load('images/standing/mario_stand0.png')
+        self.rect = self.image.get_rect()
+        self.screen_rect = self.screen.get_rect()
+
+        # Position Mario
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+        # Store decimal value of Mario's position
+        self.center = float(self.rect.centerx)
+
+        # Movement Flags
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update Mario's position"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.centerx += 1
+        if self.moving_left and self.rect.left > 0:
+            self.rect.centerx -= 1
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
+        pass
