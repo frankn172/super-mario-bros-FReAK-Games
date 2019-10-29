d4987144535315b3627801da1a8cddd565988ec2
giangkenny <55575054+giangkenny@users.noreply.github.com>
2019-10-28 23:32:19 -0700

    Added back files

---
+++ venv/main.py
@@ -0,0 +1,55 @@
+import pygame
+import constants as c
+from player import Mario
+from enemies import *
+from backgroud import Background
+import gamefunctions as gf
+from pygame.sprite import Group
+
+
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode((300, 300))
+    pygame.display.set_caption("Insert Title Here")
+    mario = Mario(screen)
+    enemies = Group()
+    enemies.add(Goomba(y=c.GROUND_HEIGHT, x=150, direction=c.LEFT))
+    enemies.add(Koopa(y=c.GROUND_HEIGHT, x=170))
+    enemies.add(Goomba(y=c.GROUND_HEIGHT, x=190, direction=c.LEFT))
+    enemies.add(Goomba(y=c.GROUND_HEIGHT, x=210, direction=c.LEFT))
+    enemies.add(Goomba(y=c.GROUND_HEIGHT, x=240, direction=c.LEFT))
+    enemies.add(Koopa(y=280, x=200, winged=True))
+    enemies.add(PirahnaPlant(y=280, x=100))
+    enemies.add(CheepCheep(y=50, x=100))
+    enemies.add(Podaboo(x=200))
+    enemies.add(FireBar(x=150,y=150))
+    enemies.add(Blooper(x=200, y=100, mario=mario))
+
+    background = Background(screen)
+    background.setTiles([["images/1-1/tile000.png",
+                          "images/1-1/tile001.png",
+                          "images/1-1/tile002.png",
+                          "images/1-1/tile003.png",
+                          "images/1-1/tile004.png",
+                          "images/1-1/tile005.png",
+                          "images/1-1/tile006.png",
+                          "images/1-1/tile007.png",
+                          "images/1-1/tile008.png",
+                          "images/1-1/tile009.png",
+                          "images/1-1/tile010.png",
+                          "images/1-1/tile011.png",
+                          "images/1-1/tile012.png",
+                          "images/1-1/tile013.png",
+                          "images/1-1/tile014.png",
+                          "images/1-1/tile015.png",
+                          "images/1-1/tile015.png"]])
+    background.scroll(0, 0)
+
+    while True:
+        gf.check_events(mario)
+        mario.update()
+        gf.update_enemies(mario, enemies)
+        gf.draw_screen(screen, mario, enemies, background)
+
+
+run_game()
